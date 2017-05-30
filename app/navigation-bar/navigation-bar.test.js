d92950e11a36ad58755d28c43ecbf80aebddc7df
Nick Nielsen <nick.nielsen@ldschurch.org>
2017-05-30 08:44:49 -0600

    app.routs removed. simplified routing. logo position set

--- app/navigation-bar/navigation-bar.test.js
+++ app/navigation-bar/navigation-bar.test.js
@@ -3,26 +3,4 @@
 describe('navigation-bar', function() {
 
   beforeEach(module('navigationBar'));
-
-  describe('navigationBarController', function(){
-
-    it('should get a and display a set of nav items', inject(function($controller, _$httpBackend_) {
-      var ctrl,
-          http = _$httpBackend_
-
-      http.expectGET('navigation/pages.json')
-                    .respond([{href: 'lesson/123'}, {href: 'lesson/321'}]);
-      
-      ctrl = $controller('navigationBarController');
-      
-      expect(ctrl).toBeDefined();
-      expect(ctrl.navigation).toEqual([]);
-
-      http.flush();
-
-      expect(ctrl.navigation).toEqual([{href: 'lesson/123'}, {href: 'lesson/321'}]);
-      expect(navList.count()).toBe(2);
-    }));
-
-  });
 });
